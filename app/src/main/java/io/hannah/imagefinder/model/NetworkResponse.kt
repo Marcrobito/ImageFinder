44833955516a6f9d47f99982bbd464f6916e762c
Marcrobito <marco.ant.mtz@gmail.com>
2024-01-31 22:11:26 -0600

    unnecessary class removed

--- app/src/main/java/io/hannah/imagefinder/model/NetworkResponse.kt
+++
@@ -1,6 +0,0 @@
-package io.hannah.imagefinder.model
-
-sealed class NetworkResponse<out T> {
-    data class Success<out T>(val data: T) : NetworkResponse<T>()
-    data class Error(val error: Exception) : NetworkResponse<Nothing>()
-}
